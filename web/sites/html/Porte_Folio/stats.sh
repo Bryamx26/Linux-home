466b8f094ab7abd66e3fb7ba8d7b4fb880adcae8
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2025-05-25 17:01:25 +0200

    mise a jour

--- web/sites/html/Porte_Folio/stats.sh
+++ web/sites/html/Porte_Folio/stats.sh
@@ -10,8 +10,9 @@ fi
 
 CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print int($2 + $4)}')
 
-GPU_TEMP="N/A"   # À adapter si tu as une commande pour le GPU
-GPU_USAGE="N/A"
+GPU_TEMP= vcgencmd measure_temp
+   # À adapter si tu as une commande pour le GPU
+
 
 RAM_USED=$(free -g | awk '/^Mem:/ {print $3}')
 RAM_TOTAL=$(free -g | awk '/^Mem:/ {print $2}')
@@ -27,17 +28,16 @@ OUTPUT="index.html"
 # Copie du template vers le fichier de sortie
 cp "$TEMPLATE" "$OUTPUT"
 
-# Remplacement des placeholders dans le fichier de sortie
+# Remplacement des placeholders dans le fichier de sortie avec | comme séparateur sed
 sed -i \
-  -e "s/{{CPU_TEMP}}/$CPU_TEMP/g" \
-  -e "s/{{CPU_USAGE}}/$CPU_USAGE/g" \
-  -e "s/{{GPU_TEMP}}/$GPU_TEMP/g" \
-  -e "s/{{GPU_USAGE}}/$GPU_USAGE/g" \
-  -e "s/{{RAM_USED}}/$RAM_USED/g" \
-  -e "s/{{RAM_TOTAL}}/$RAM_TOTAL/g" \
-  -e "s/{{RAM_USAGE}}/$RAM_USAGE/g" \
-  -e "s/{{USERS_CONNECTED}}/$USERS_CONNECTED/g" \
-  -e "s/{{USERS_ACTIVE}}/$USERS_ACTIVE/g" \
+  -e "s|{{CPU_TEMP}}|$CPU_TEMP|g" \
+  -e "s|{{CPU_USAGE}}|$CPU_USAGE|g" \
+  -e "s|{{GPU_TEMP}}|$GPU_TEMP|g" \
+  -e "s|{{RAM_USED}}|$RAM_USED|g" \
+  -e "s|{{RAM_TOTAL}}|$RAM_TOTAL|g" \
+  -e "s|{{RAM_USAGE}}|$RAM_USAGE|g" \
+  -e "s|{{USERS_CONNECTED}}|$USERS_CONNECTED|g" \
+  -e "s|{{USERS_ACTIVE}}|$USERS_ACTIVE|g" \
   "$OUTPUT"
 
 echo "Le fichier $OUTPUT a été mis à jour avec les données système."
