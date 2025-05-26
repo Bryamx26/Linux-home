005988bc0a780b12b167c2cb5b2fd66087b9963d
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2025-05-26 22:11:59 +0200

    Update music_s.js

--- web/sites/html/Porte_Folio/Music_s/music_s.js
+++ web/sites/html/Porte_Folio/Music_s/music_s.js
@@ -19,7 +19,7 @@ function convertir() {
   const isLocal = (
     hostname === 'localhost' ||
     hostname.startsWith('192.168.') ||
-    hostname.startsWith('10.') ||
+   
     /^172\.(1[6-9]|2[0-9]|3[01])\./.test(hostname)
   );
 
